eded1b566fdb9d9dfeae1b5b314ec5d0bfa98ce1
Daniel Barclay <barclaysd@me.com>
2019-01-31 23:55:41 +0000

    Player class with keyboard bindings to move player in all directions

--- maze.py
+++ maze.py
@@ -5,6 +5,7 @@ wn = turtle.Screen()
 wn.bgcolor('#2F4F4F')
 wn.title('Maze Game')
 wn.setup(width=700, height=700)
+wn.tracer(0)
 
 
 # classes
@@ -17,12 +18,31 @@ class Pen(turtle.Turtle):
         self.speed(0)
 
 
+class Player(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.speed(0)
+        self.shape('circle')
+        self.color('#19212a')
+
+    def move_up(self):
+        self.setposition(self.xcor(), self.ycor() + 24)
+
+    def move_down(self):
+        self.setposition(self.xcor(), self.ycor() - 24)
+
+    def move_left(self):
+        self.setposition(self.xcor() - 24, self.ycor())
+
+    def move_right(self):
+        self.setposition(self.xcor() + 24, self.ycor())
+
 # game status
-levels = []
+levelsList = []
 
 # levels
-levels.append(level_1)
-
+levelsList.append(level_1)
 
 # functions
 def setup_maze(level):
@@ -40,14 +60,26 @@ def setup_maze(level):
                 pen.goto(screen_x, screen_y)
                 pen.stamp()
 
+            if character == 'P':
+                player.setposition(screen_x, screen_y)
+
 
 # class instances
 pen = Pen()
+player = Player()
+
+# keyboard bindings
+wn.listen()
+wn.onkeypress(player.move_up, "Up")
+wn.onkeypress(player.move_down, "Down")
+wn.onkeypress(player.move_left, "Left")
+wn.onkeypress(player.move_right, "Right")
+
 
 # append levels to levels list
-
-setup_maze(level_1)
+setup_maze(levelsList[0])
 
 # main loop
 while True:
+    wn.update()
     pass
